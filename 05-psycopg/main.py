855b3df8658a29a86ce7262740d52b8a9048251d
administrator <P@ssw0rd>
2022-09-20 12:38:06 +0300

    05-psycopg

--- 05-psycopg/main.py
+++ 05-psycopg/main.py
@@ -2,59 +2,219 @@ import psycopg2
 
 
 def init_client_db(database_name, user_name, user_password):
-    conn = psycopg2.connect(database=database_name, user=user_name, password=user_password)
+    conn = psycopg2.connect(
+        database=database_name, user=user_name, password=user_password
+    )
     with conn.cursor() as cur:
-        cur.execute("""
-                    DROP TABLE phone;
-                    DROP TABLE client;
-                """)
-        cur.execute("""
+        # cur.execute(
+        #     """
+        #             DROP TABLE phone;
+        #             DROP TABLE client;
+        #         """
+        # )
+        cur.execute(
+            """
                     CREATE TABLE IF NOT EXISTS client(
                         client_id SERIAL NOT NULL PRIMARY KEY, 
                         name varchar(40) NOT NULL,
                         surname varchar(40) NOT NULL,
-                        email varchar(40) NOT NULL
+                        email varchar(40) UNIQUE NOT NULL
                     );
-                """)
-        cur.execute("""
+                """
+        )
+        cur.execute(
+            """
                     CREATE TABLE IF NOT EXISTS phone(
                         phone_id SERIAL NOT NULL PRIMARY KEY,
                         phone varchar(40) UNIQUE,
                         client_id INT REFERENCES client(client_id) NOT NULL
                         );
-                """)
+                """
+        )
     conn.commit()
     return conn
 
 
 def add_client(conn, name, surname, email, *phones):
     with conn.cursor() as cur:
-        cur.execute("""
+        cur.execute(
+            """
                     INSERT INTO client(name, surname, email) VALUES(%s, %s, %s) RETURNING client_id
                     """,
-                    (name, surname, email))
+            (name, surname, email),
+        )
         client_id = cur.fetchone()[0]
         for phone in phones:
-            cur.execute("""
+            cur.execute(
+                """
                         INSERT INTO phone(phone, client_id) VALUES(%s, %s)
                         """,
-                        (phone, client_id))
+                (phone, client_id),
+            )
         return client_id
 
 
-db_conn = init_client_db(database_name="netology_db", user_name="postgres", user_password="P@ssw0rd")
-add_client(db_conn, 'Ivan', 'Ivanov', 'ivanov@mail.ru', '89991776361')
-add_client(db_conn, 'Petr', 'Petrov', 'petrov@mail.ru', '89991776362', '89996203042')
+def check_client_email(conn, email):
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+                    SELECT client_id, email FROM client;
+                    """
+        )
+        for item in cur.fetchall():
+            client_id, client_email = item
+            if email == client_email:
+                return [True, client_id]
+    return [False, None]
 
-with db_conn.cursor() as cur:
-    cur.execute("""
-    SELECT * FROM client;
-    """)
-    print(cur.fetchall())
 
-    cur.execute("""
-        SELECT * FROM phone;
-        """)
-    print(cur.fetchall())
+def add_client_phone(conn, email, *phones):
+    mail_check_result, client_id = check_client_email(conn, email)
+    if mail_check_result:
+        with conn.cursor() as cur:
+            for phone in phones:
+                cur.execute(
+                    """
+                            INSERT INTO phone(phone, client_id) VALUES(%s, %s)
+                            """,
+                    (phone, client_id),
+                )
+            return client_id
+
+
+def change_client_data(conn, email, data):
+    # data = {'name': [flag,name], 'surname': [flag, surname], 'email': [flag, email], 'phone': [flag, [phone_1, ..., ]}
+    mail_check_result, client_id = check_client_email(conn, email)
+    if mail_check_result:
+        with conn.cursor() as cur:
+            if data["name"][0]:
+                new_name = data["name"][1]
+                cur.execute(
+                    """
+                        UPDATE client SET name=%s WHERE client_id=%s;
+                        """,
+                    (new_name, client_id),
+                )
+            if data["surname"][0]:
+                new_surname = data["surname"][1]
+                cur.execute(
+                    """
+                        UPDATE client SET surname=%s WHERE client_id=%s;
+                        """,
+                    (new_surname, client_id),
+                )
+            if data["email"][0]:
+                new_email = data["email"][1]
+                cur.execute(
+                    """
+                        UPDATE client SET email=%s WHERE client_id=%s;
+                        """,
+                    (new_email, client_id),
+                )
+            if data["phone"][0]:
+                phones = data["phone"][1]
+                cur.execute(
+                    """
+                       DELETE FROM phone WHERE client_id=%s;
+                       """,
+                    (client_id,),
+                )
+                for phone in phones:
+                    cur.execute(
+                        """
+                                INSERT INTO phone(phone, client_id) VALUES(%s, %s)
+                                """,
+                        (phone, client_id),
+                    )
+            conn.commit()
+            return client_id
+
+
+def delete_client_phone(conn, email):
+    mail_check_result, client_id = check_client_email(conn, email)
+    if mail_check_result:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                   DELETE FROM phone WHERE client_id=%s;
+                   """,
+                (client_id,),
+            )
+            conn.commit()
+            return client_id
+
+
+def delete_client(conn, email):
+    mail_check_result, client_id = check_client_email(conn, email)
+    if mail_check_result:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                   DELETE FROM phone WHERE client_id=%s;
+                   """,
+                (client_id,),
+            )
+            cur.execute(
+                """
+                   DELETE FROM client WHERE client_id=%s;
+                   """,
+                (client_id,),
+            )
+            conn.commit()
+            return client_id
+
+
+db_conn = init_client_db(
+    database_name="netology_db", user_name="postgres", user_password="P@ssw0rd"
+)
+add_client(db_conn, "Ivan", "Ivanov", "ivanov@mail.ru", "89991776361")
+add_client(db_conn, "Fedor", "Fedorov", "fedorov@mail.ru", "89991776364")
+add_client(db_conn, "Petr", "Petrov", "petrov@mail.ru", "89991776362", "89996203042")
+add_client(db_conn, "Vasiliy", "Vasiliev", "vasiliev@mail.ru")
+
+with db_conn.cursor() as db_cur:
+    db_cur.execute(
+        """
+                SELECT * FROM client;
+                """
+    )
+    print(db_cur.fetchall())
+
+    db_cur.execute(
+        """
+                SELECT * FROM phone;
+                """
+    )
+    print(db_cur.fetchall())
+
+add_client_phone(db_conn, "fedorov@mail.ru", "89991776365")
+add_client_phone(db_conn, "vasiliev@mail.ru", "89991776369")
+change_client_data(
+    db_conn,
+    "petrov@mail.ru",
+    {
+        "name": [True, "Vladimir"],
+        "surname": [True, "Vladimirov"],
+        "email": [True, "vladimirov@mail.ru"],
+        "phone": [True, ["776415"]],
+    },
+)
+delete_client_phone(db_conn, "vasiliev@mail.ru")
+delete_client(db_conn, "vasiliev@mail.ru")
+
+with db_conn.cursor() as db_cur:
+    db_cur.execute(
+        """
+                SELECT * FROM client;
+                """
+    )
+    print(db_cur.fetchall())
+
+    db_cur.execute(
+        """
+                SELECT * FROM phone;
+                """
+    )
+    print(db_cur.fetchall())
 
 db_conn.close()

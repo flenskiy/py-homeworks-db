ed281aaa46bd43da1cb09f5ee2f278cd3a06c367
administrator <P@ssw0rd>
2022-09-30 16:35:37 +0300

    06-orm

--- 06-orm/main.py
+++ 06-orm/main.py
@@ -1,3 +1,4 @@
+import json
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from models import create_tables, Publisher, Shop, Book, Stock, Sale
@@ -10,14 +11,56 @@ create_tables(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-publisher1 = Publisher(name="Vega")
-publisher2 = Publisher(name="Izdanie")
+with open("fixtures/tests_data.json", "r") as f:
+    data = json.load(f)
 
-session.add(publisher1)
-session.add(publisher2)
+for record in data:
+    model = {
+        "publisher": Publisher,
+        "shop": Shop,
+        "book": Book,
+        "stock": Stock,
+        "sale": Sale,
+    }[record.get("model")]
+    session.add(model(id=record.get("pk"), **record.get("fields")))
 session.commit()
 
-print(publisher1.id, publisher1.name)
-print(publisher2.id, publisher2.name)
+publisher_id = None
+publisher_name = None
+user_input = input("Enter publisher name or id: ")
+try:
+    publisher_id = int(user_input)
+except ValueError:
+    publisher_name = user_input
+result = (
+    session.query(Publisher)
+    .filter(
+        (Publisher.name.ilike(f"{publisher_name}") | (Publisher.id == publisher_id))
+    )
+    .first()
+)
+print(result)
+
+# for item in session.query(Publisher).filter(Publisher.name.ilike(f"{publisher}")).all():
+#     print(item)
+# publisher1 = Publisher(name="Vega")
+# publisher2 = Publisher(name="Izdanie")
+#
+# session.add(publisher1)
+# session.add(publisher2)
+# session.commit()
+#
+# print(publisher1.id, publisher1.name)
+# print(publisher2.id, publisher2.name)
+# print(publisher1)
+#
+# book1 = Book(title="Book1", publisher_id=1)
+# book2 = Book(title="Book2", publisher_id=2)
+# book3 = Book(title="Book3", publisher_id=1)
+# session.add_all([book1, book2, book3])
+# session.commit()
+#
+# for book in session.query(Book).all():
+#     print(book)
 
 session.close()
--- 06-orm/models.py
+++ 06-orm/models.py
@@ -10,6 +10,9 @@ class Publisher(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40))
 
+    def __str__(self):
+        return f"Publisher {self.id}: {self.name}"
+
 
 class Shop(Base):
     __tablename__ = "shop"
@@ -23,20 +26,23 @@ class Book(Base):
 
     id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=40), nullable=False)
-    publisher_id = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
 
     publisher = relationship(Publisher, backref="book")
+
+    def __str__(self):
+        return f"Book {self.id}: ({self.title}, {self.id_publisher})"
 
 
 class Stock(Base):
     __tablename__ = "stock"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    book_id = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
-    shop_id = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
-    book = relationship(Book, backref="stock")
+    books = relationship(Book, backref="stock")
     shop = relationship(Shop, backref="stock")
 
 
@@ -46,10 +52,10 @@ class Sale(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     price = sq.Column(sq.String(length=40), nullable=False)
     date_sale = sq.Column(sq.Date, nullable=False)
-    stock_id = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
-    stock = relationship(Stock, backref="sale")
+    stock = relationship(Stock, backref="sales")
 
 
 def create_tables(engine):
